83335c531bd740cb3c1b74889d0a76c5711080ff
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-02-01 17:56:54 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,17 @@
 module Memorable 
+  module ClassMethods 
+    def reset_all
+      self.all.clear
+    end 
 
-def reset_all
-  all.clear
-end 
-
-def count
-  all.count
-end 
+    def count
+      self.all.count
+    end 
+    
+    module InstanceMethods 
+      def initialize 
+        
+      end 
+    end
 
 end 
